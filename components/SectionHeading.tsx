4052a1169b16d43f9026665ef360e2197f8254c7
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-07-09 15:48:25 +0900

    update

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -1,5 +1,11 @@
 
-export default function SectionHeading() {
+type SectionHeadingProps = {
+  eyebrow?: string
+  title?: string
+  description?: string
+}
+
+export default function SectionHeading({ eyebrow, title, description }: SectionHeadingProps) {
   return (
     <div>
 
