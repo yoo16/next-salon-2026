e6ca6458c2769eb3f47190efc4f5e73b29845035
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-09-03 15:47:19 +0900

    update

---
+++ components/ShopCard.tsx
@@ -0,0 +1,15 @@
+// rfc スニペットを使う
+// Shop型ファイルをインポート
+import { Shop } from '@/types/shop'
+
+type ShopCardProps = {
+    shop: Shop
+}
+
+export default function ShopCard({ shop } : ShopCardProps) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
