d05f553c57de7a1e991b7383281f62e4645a48b3
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-09-10 15:36:08 +0900

    update

--- app/api/shops/route.ts
+++ app/api/shops/route.ts
@@ -1,6 +1,7 @@
 import { MOCK_SHOPS } from "@/data/MockData";
 import { NextResponse } from "next/server";
 
+// http://localhost:3000/api/shops
 // GETリクエスト（非同期通信）
 export async function GET() {
     // TODO: DBからショップデータを取得
