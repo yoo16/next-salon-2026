1794b7a2062fb60136d9b888ea75b60817fac49c
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-09-17 15:07:32 +0900

    update

--- app/api/shops/[id]/route.ts
+++ app/api/shops/[id]/route.ts
@@ -1,9 +1,11 @@
 import { MOCK_SHOPS } from "@/data/MockData";
 import { NextResponse } from "next/server";
 
+type Params = { params: Promise<{ id: string }> }
+
 // http://localhost:3000/api/shops/[id]
 // GETリクエスト（非同期通信）
-export async function GET() {
+export async function GET({ params }: Params) {
     // TODO: DBからショップデータを取得
     const shops = MOCK_SHOPS
 
