9194db22415b52e4205ccb7f52b32e2c93f65fc7
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-09-17 14:56:23 +0900

    update

---
+++ app/api/shops/[id]/route.ts
@@ -0,0 +1,12 @@
+import { MOCK_SHOPS } from "@/data/MockData";
+import { NextResponse } from "next/server";
+
+// http://localhost:3000/api/shops/[id]
+// GETリクエスト（非同期通信）
+export async function GET() {
+    // TODO: DBからショップデータを取得
+    const shops = MOCK_SHOPS
+
+    // JSONでレスポンス
+    return NextResponse.json({ shops })
+}
