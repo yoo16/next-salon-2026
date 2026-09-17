260af40e0def2fdd60e83dc9f87086144fe6de32
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-09-17 16:19:59 +0900

    update

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { getShop } from '@/lib/services/shops';
 
-export default async function ShopDetailPage() {
+type Props = {
+    params: Promise<{ id: string }>
+}
+
+export default async function ShopDetailPage({ params }: Props) {
+    const { id } = await params
+    const shop = await getShop(id)
+
     return (
         <div>
             <h1>Shop Details</h1>
+            <p>{shop.name}</p>
         </div>
     )
 }
